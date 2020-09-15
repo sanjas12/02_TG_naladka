c90cf5310964ca64629a0b9dee96aeb4cfa9acd0
stovpyaga <sanja100v@gmail.com>
2020-09-15 14:01:01 +0300

    update static_9

--- grath_from_tg_static_9.py
+++ grath_from_tg_static_9.py
@@ -5,44 +5,53 @@ Created on Tue Aug 18 11:25:26 2020
 @author: sanja_s
 """
 
-import tkinter as tk
-from tkinter import filedialog as fd
-import pandas as pd
-#import PyQt4
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
 
-    def createWidgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Open GZ files"
-        self.hi_there["command"] = self.open_gz
-        self.hi_there.pack(side="top")
-        
-#        self.Button_print = tk.Button(self)
-#        self.Button_print['text'] = 'print'
-#        self.Button_print["command"] = self.print_data
-#        self.Button_print.pack(side="top")
-        
-#        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-#                                            command=root.destroy)
-#        self.QUIT.pack(side="bottom")
+class Filedialogdemo(QWidget):
+    def __init__(self, parent=None):
+        super(Filedialogdemo, self).__init__(parent)
+
+        layout = QVBoxLayout()
+        self.btn1 = QPushButton("Open GZ files")
+        self.btn1.clicked.connect(self.open_gz)
+        layout.addWidget(self.btn1)
+
+        self.setLayout(layout)
+        self.setWindowTitle("File Dialog")
 
     def open_gz(self):
-        
-        
-        self.gz_files = fd.askopenfiles(title='Открыть GZ файл', 
-                                       filetypes=[('GZ files', '*.gz'), ('CSV files', '*.csv')], initialdir='')
-        
-        # Считывание названия всех колонок
-        name_column = pd.read_csv(self.gz_files[0], delim_whitespace='\t', nrows=0)
-        print(name_column)
-        
-        
-#       print(self.csv_file)
+        # self.files = QFileDialog.getOpenFileNames(self, 'Open GZ files', '*.gz')
+        fname = QFileDialog.getOpenFileName(self, 'Open file', "Open GZ files (*.gz)")
+        # print(len(self.files[0]))
+
+        # dlg = QFileDialog()
+        # dlg.setFileMode(QFileDialog.AnyFile)
+        # dlg.setFilter("Text files (*.txt)")
+        # filenames = QStringList()
+        # if dlg.exec_():
+        #     filenames = dlg.selectedFiles()
+        # f = open(filenames[0], 'r')
+        # with f:
+        #     data = f.read()
+        # self.contents.setText(data)
+
+
+
+
+
+        # self.gz_files = fd.askopenfiles(title='Открыть GZ файл',
+        #                                filetypes=[('GZ files', '*.gz'), ('CSV files', '*.csv')], initialdir='')
+        # print(self.gz_files[0])
+        #
+        # # Считывание названия всех колонок
+        # name_column = pd.read_csv(self.gz_files[0], delim_whitespace='\t', nrows=0)
+        # print(name_column)
+
+
+# print(self.csv_file)
 #       print(self.b)
 #        for file_ in self.gz_files:
 #            print(file_)
@@ -50,6 +59,12 @@ class Application(tk.Frame):
 #        columns_csv = pd.read_csv(self.csv_file, header=0)
 #        print(df.info)        
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+def main():
+    app = QApplication(sys.argv)
+    ex = Filedialogdemo()
+    ex.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
