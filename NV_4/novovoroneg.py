e112e7bbd0974611350295d8f3aacf52523ee800
sanja <sanja100v@gmail.com>
2022-11-25 17:24:09 +0300

    Add NVAES

---
+++ NV_4/novovoroneg.py
@@ -0,0 +1,26 @@
+from pyModbusTCP.client import ModbusClient 
+import time
+
+
+try:
+    # c = ModbusClient(host='localhost', port=502)
+    c = ModbusClient(host='192.168.30.111', port=502)
+except ValueError:
+    print("Error with host or port params")
+
+data = [0, 0, 0, 0]
+
+while True:
+    time.sleep(1)
+    w = c.write_multiple_registers(regs_addr=15, regs_value=data)
+    data[0] = data[0] + 1
+    if data[0] >= 99:
+        data[0] = 1
+    data[1] = data[1] + 2
+    if data[1] >= 99:
+        data[1] = 1
+    data[2] = data[2] + 3
+    if data[2] >= 99:
+        data[2] = 1
+    r = c.read_input_registers(reg_addr=15, reg_nb=len(data))
+    print(r)
