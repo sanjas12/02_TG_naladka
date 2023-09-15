ecceb6d23fd3d4979479c33487278840f2e42e58
diakont_notebook <sanja100v@gmail.com>
2023-09-15 15:28:45 +0300

    Add version number in format X.X.X.X

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#0.0.1.0
 import sys
 from cx_Freeze import setup, Executable
 
@@ -12,7 +13,7 @@ base = "Win32GUI" if sys.platform == "win32" else None4
 
 setup(
     name="TG-Naladka",
-    version="0.1",
+    version="0.0.1.0",
     description="My TG-Naladka",
     options={"build_exe": build_exe_options},
     executables=[Executable("main.py", base=base)],
